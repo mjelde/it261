fbff9e928803ad79811ecb7df7b66e7173187561
Sharon Alexander <sharonalexander22@outlook.com>
2020-11-10 11:12:32 -0800

    adding index, thx, form
    adding index, thx, form

---
+++ thx.php
@@ -0,0 +1,24 @@
+<?php 
+include('config.php'); 
+include('includes/header.php');
+
+?>
+
+
+
+    <div id="wrapper">
+
+<main>
+<h1><?php echo $mainHeadline; ?></h1>
+
+</main>
+        
+<aside>
+<p>Come back and see us!</p>        
+
+</aside>
+
+
+    
+<?php include('includes/footer.php');
+?>
