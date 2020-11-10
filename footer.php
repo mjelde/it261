569c1da09e93fbaf1e6b52305ebf52da9b686b45
Sharon Alexander <sharonalexander22@outlook.com>
2020-11-10 10:29:07 -0800

    Adding Wk6 Header, footer, and contact
    Adding Wk6 Header, footer, and contact

---
+++ footer.php
@@ -0,0 +1,14 @@
+ 
+<footer>
+        <ul>
+            <li>Copyright <?php echo date('Y'); ?></li>
+            <li>All Rights Reserved</li>
+            <li><a href="https://www.linkedin.com/in/lcared/">Web Design by Sharon</a></li>
+            <li><a href="http://validator.w3.org/check/referer">HTML</a></li>
+            <li><a href="http://jigsaw.w3.org/css-validator/check?uri=referer">CSS</a></li>
+        </ul>
+    
+    </footer>
+    </div> <!--end wrapper-->
+</body>
+</html>
